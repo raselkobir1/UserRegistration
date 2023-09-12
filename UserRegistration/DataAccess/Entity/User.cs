c4c2ba291ce44c31a67f1aa6736c806761be49e5
Rasel <USER@DESKTOP-LPSJO96>
2023-09-12 17:53:47 +0600

    add user class

--- UserRegistration/DataAccess/Entity/User.cs
+++ UserRegistration/DataAccess/Entity/User.cs
@@ -2,5 +2,12 @@
 {
     public class User
     {
+        public long Id { get; set; }
+        public string FullName { get; set; } 
+        public string UserName { get; set; }  //unique 6 char only number and string
+        public string Password { get; set; }  // hash long 8 char
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }  //only numeric no space
     }
 }
